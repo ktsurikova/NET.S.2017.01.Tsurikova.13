439f11876fc99468bd7eefb1101834cd5dbcd713
user <kionopia@gmail.com>
2017-07-27 11:46:29 +0300

    xml comments added

--- NET.S.2017.01.Tsurikova.13/Fibonacci/FibonacciSequence.cs
+++ NET.S.2017.01.Tsurikova.13/Fibonacci/FibonacciSequence.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Fibonacci
 {
+    /// <summary>
+    /// class for working with fibonacci numbers
+    /// </summary>
     public class FibonacciSequence
     {
+        /// <summary>
+        /// generate sequence of fibonacci numbers
+        /// </summary>
+        /// <param name="number">number to which sequence to be generated</param>
+        /// <returns>sequence of numbers</returns>
+        /// <exception cref="ArgumentException">throws when number is negative</exception>
         public static IEnumerable<int> Generate(int number)
         {
             if (number < 0) throw new ArgumentException($"{nameof(number)} can't be negative");
