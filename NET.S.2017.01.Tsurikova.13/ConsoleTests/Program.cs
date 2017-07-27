543a8e6100d738c4a38249df3fe3a0e719ba9387
user <kionopia@gmail.com>
2017-07-27 11:42:39 +0300

    fibonacci sequence added

--- NET.S.2017.01.Tsurikova.13/ConsoleTests/Program.cs
+++ NET.S.2017.01.Tsurikova.13/ConsoleTests/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fibonacci;
 using Queue;
 
 namespace ConsoleTests
@@ -10,26 +11,46 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
-            Queue<string> queue= new Queue<string>(new []{"cat Ginger", "cat Vasya", "dog Rex"});
-            Console.WriteLine($"3 {queue.Count}");
-            queue.Enqueue("crocodile Gena");
-            Console.WriteLine($"Ginger {queue.Peek()}");
-            Console.WriteLine($"Ginger {queue.Dequeue()}");
-            Console.WriteLine();
-            foreach (var item in queue)
+            //Queue<string> queue= new Queue<string>(new []{"cat Ginger", "cat Vasya", "dog Rex"});
+            //Console.WriteLine($"3 {queue.Count}");
+            //queue.Enqueue("crocodile Gena");
+            //Console.WriteLine($"Ginger {queue.Peek()}");
+            //Console.WriteLine($"Ginger {queue.Dequeue()}");
+            //Console.WriteLine();
+            //foreach (var item in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine($"true {queue.Contains("cat Vasya")}");
+            //Console.WriteLine();
+            //string[] animals = queue.ToArray();
+            //Array.ForEach(animals, Console.WriteLine);
+            //Console.WriteLine();
+            //animals[0] = null;
+            //queue.СоруТо(animals, 1);
+            //Array.ForEach(animals, Console.WriteLine);
+            //queue.Clear();
+            //Console.WriteLine(queue.Count);
+
+            foreach (var VARIABLE in FibonacciSequence.Generate(0))
             {
-                Console.WriteLine(item);
+                Console.WriteLine(VARIABLE);
             }
-            Console.WriteLine($"true {queue.Contains("cat Vasya")}");
             Console.WriteLine();
-            string[] animals = queue.ToArray();
-            Array.ForEach(animals, Console.WriteLine);
+            foreach (var VARIABLE in FibonacciSequence.Generate(1))
+            {
+                Console.WriteLine(VARIABLE);
+            }
             Console.WriteLine();
-            animals[0] = null;
-            queue.СоруТо(animals, 1);
-            Array.ForEach(animals, Console.WriteLine);
-            queue.Clear();
-            Console.WriteLine(queue.Count);
+            foreach (var VARIABLE in FibonacciSequence.Generate(10))
+            {
+                Console.WriteLine(VARIABLE);
+            }
+            Console.WriteLine();
+            foreach (var VARIABLE in FibonacciSequence.Generate(200))
+            {
+                Console.WriteLine(VARIABLE);
+            }
 
             Console.ReadLine();
         }
---
+++ NET.S.2017.01.Tsurikova.13/Fibonacci/FibonacciSequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class FibonacciSequence
+    {
+        public static IEnumerable<int> Generate(int number)
+        {
+            if (number < 0) throw new ArgumentException($"{nameof(number)} can't be negative");
+            int a = 0;
+            int b = 1;
+
+            yield return a;
+            if (number == 0) yield break;
+            yield return b;
+
+            while (a + b <= number)
+            {
+                int sum = a + b;
+                a = b;
+                b = sum;
+                yield return sum;
+            }
+        }
+    }
+}
